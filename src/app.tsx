57b3699591ce5dd712d4d039c25a8ef05a38b9bd
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-09-11 19:16:03 -0300

    Feat: Dialog sketch

--- src/app.tsx
+++ src/app.tsx
@@ -1,29 +1,81 @@
 import logo from './assets/logo-in-orbit.svg'
 import letStart from './assets/lets-start.svg'
-import { Plus } from 'lucide-react'
+import { Plus, X } from 'lucide-react'
 import { Button } from './components/ui/button'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from './components/ui/dialog'
+import { Label } from './components/ui/label'
+import { Input } from './components/ui/input'
+import { RadioGroup, RadioGroupItem } from './components/ui/radio-group'
 
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in.orbit logo" />
-      <img src={letStart} alt="in.orbit logo" />
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-      </p>
+    <Dialog>
+      <div className="h-screen flex flex-col items-center justify-center gap-8">
+        <img src={logo} alt="in.orbit logo" />
+        <img src={letStart} alt="in.orbit logo" />
+        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+          Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora
+          mesmo?
+        </p>
 
-      <Button>
-        <Plus className="size-4" />
-        Cadastrar meta
-      </Button>
+        <DialogTrigger asChild>
+          <Button>
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
 
-      {/* <button
-        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600 transition duration-250"
-        type="button"
-      >
-        <Plus className="size-4" />
-        Cadastrar meta
-      </button> */}
-    </div>
+      <DialogContent>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-3">
+            <div className="flex items-center justify-between ">
+              <DialogTitle>Cadastrar Meta</DialogTitle>
+              <DialogClose>
+                <X className="size-5 text-zinc-600" />
+              </DialogClose>
+            </div>
+            <DialogDescription>
+              Adicione atividades que te fazem bem e que você quer continuar
+              praticando toda semana.
+            </DialogDescription>
+          </div>
+          <form action="" className="flex-1 flex-col justify-between">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-2">
+                <Label htmlFor="title">Qual a atividade ?</Label>
+                <Input
+                  placeholder="Praticar exercícios, meditar, etc...."
+                  autoFocus
+                  id="title"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <Label htmlFor="title">Quantas vezes na semana ?</Label>
+                <RadioGroup>
+                  <RadioGroupItem value="1">1x na semana</RadioGroupItem>
+                </RadioGroup>
+              </div>
+            </div>
+
+            <div className="mt-auto flex items-center gap-3">
+              <DialogClose asChild>
+                <Button type="button" className="flex-1" variant="secondary">
+                  Fechar
+                </Button>
+              </DialogClose>
+              <Button className="flex-1">Salvar</Button>
+            </div>
+          </form>
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
