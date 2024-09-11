fa04f8a3c98ec273819c9ea29b08822773c75aa9
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-09-11 18:39:33 -0300

    Refac: adjust transition to button hover

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import logo from './assets/logo-in-orbit.svg'
 import letStart from './assets/lets-start.svg'
 import { Plus } from 'lucide-react'
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
@@ -11,13 +12,18 @@ export function App() {
         Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
       </p>
 
-      <button
+      <Button>
+        <Plus className="size-4" />
+        Cadastrar meta
+      </Button>
+
+      {/* <button
         className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600 transition duration-250"
         type="button"
       >
         <Plus className="size-4" />
         Cadastrar meta
-      </button>
+      </button> */}
     </div>
   )
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,7 +2,7 @@ import { forwardRef, type ComponentProps } from 'react'
 import { tv, type VariantProps } from 'tailwind-variants'
 
 const button = tv({
-  base: 'flex items-center justify-center gap-2 rounded-lg text-sm font-medium tracking-tight outline-none ring-offset-2 ring-offset-black focus-visible:ring-2',
+  base: 'flex items-center justify-center gap-2 rounded-lg text-sm font-medium tracking-tight outline-none ring-offset-2 ring-offset-black focus-visible:ring-2 transition duration-200',
 
   variants: {
     variant: {
