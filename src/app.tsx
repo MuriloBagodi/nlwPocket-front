b9e3a21b3cc52013fb90b4ac05b14ce222562c82
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-09-12 09:54:13 -0300

    Refac: create empryGoals component

--- src/app.tsx
+++ src/app.tsx
@@ -1,43 +1,11 @@
-import logo from './assets/logo-in-orbit.svg'
-import letStart from './assets/lets-start.svg'
-import { Plus, X } from 'lucide-react'
-import { Button } from './components/ui/button'
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogTitle,
-  DialogTrigger,
-} from './components/ui/dialog'
-import { Label } from './components/ui/label'
-import { Input } from './components/ui/input'
-import {
-  RadioGroup,
-  RadioGroupIndicator,
-  RadioGroupItem,
-} from './components/ui/radio-group'
+import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
+import { EmptyGoals } from './components/empty-goals'
 
 export function App() {
   return (
     <Dialog>
-      <div className="h-screen flex flex-col items-center justify-center gap-8">
-        <img src={logo} alt="in.orbit logo" />
-        <img src={letStart} alt="in.orbit logo" />
-        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-          Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora
-          mesmo?
-        </p>
-
-        <DialogTrigger asChild>
-          <Button>
-            <Plus className="size-4" />
-            Cadastrar meta
-          </Button>
-        </DialogTrigger>
-      </div>
-
+      <EmptyGoals />
       <CreateGoal />
     </Dialog>
   )
